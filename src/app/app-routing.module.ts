b8460821aa80063b775487187f92a17acbbc9821
J-Niedziolka <jn85638@stud.uph.edu.pl>
2022-11-20 20:40:43 +0100

    init, prepared components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { HomeComponent } from './home/home.component';
+import { ContactComponent } from './contact/contact.component';
+import { TodoComponent } from './todo/todo.component';
+import { PrivateComponent } from './private/private.component';
+
+const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  {  path: 'contact', component: ContactComponent },  
+  {  path: 'todo', component: TodoComponent },
+  {  path: 'private', component: PrivateComponent },
+  { path: ' ', redirectTo: '/home', pathMatch: 'full'},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { ContactComponent } from './contact/contact.component';
+import { TodoComponent } from './todo/todo.component';
+import { PrivateComponent } from './private/private.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ContactComponent,
+    TodoComponent,
+    PrivateComponent
   ],
   imports: [
     BrowserModule,
